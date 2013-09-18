43a3deb5c386314780eb36ba4935ed35945b97d8
half-ogre <nemo@half-ogre.com>
2013-09-18 13:31:44 -0700

    cloth armor sprite

---
+++ MonoGameQuest/Sprites/ClothArmor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGameQuest.Sprites
+{
+    public class ClothArmor : Sprite
+    {
+        public ClothArmor(ContentManager contentManager)
+            : base(contentManager.Load<Texture2D>("images/1/clotharmor"), 32, 32, -8, -12)
+        {
+            AddAnimation(new Animation(AnimationIdentifier.IdleDown, 8, 2, Animation.DefaultIdleSpeed));
+        }
+    }
+}
