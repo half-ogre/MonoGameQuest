e9761e8aa361370a2c5265fd9ef8f964a03f9314
half-ogre <nemo@half-ogre.com>
2013-09-19 22:38:12 -0700

    smoother movement

--- MonoGameQuest/Constants.cs
+++ MonoGameQuest/Constants.cs
@@ -4,7 +4,7 @@
     {
         public const int DefaultAttackSpeed = 50;
         public const int DefaultIdleSpeed = 450;
-        public const int DefaultMoveLength = 4;
+        public const int DefaultMoveLength = 8;
         public const int DefaultMoveSpeed = 120;
         public const int DefaultWalkSpeed = 100;
     }
