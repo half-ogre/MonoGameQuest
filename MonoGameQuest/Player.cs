d2da420e4eb26b2c48c48bdf257c087d316fa18d
half-ogre <nemo@half-ogre.com>
2013-09-28 20:38:59 -0700

    set the player's path on mouse click

--- MonoGameQuest/Player.cs
+++ MonoGameQuest/Player.cs
@@ -1,14 +1,20 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
 namespace MonoGameQuest
 {
     public class Player : MonoGameQuestComponent
     {
+        bool _leftMouseButtonWasPressed;
+        readonly Pathfinder _pathfinder;
         PlayerSprite _sprite;
 
         public Player(MonoGameQuest game) : base(game)
         {
+            _pathfinder = new Pathfinder();
+            
+            Path = new Queue<Vector2>();
             UpdateOrder = Constants.UpdateOrder.Models;
         }
 
@@ -22,8 +28,34 @@ namespace MonoGameQuest
 
         public Vector2 CoordinatePosition { get { return _sprite.CoordinatePosition; } }
 
+        public void Move(Vector2 destination)
+        {
+            // if the player is not currently traveling a path, use the current coordinate position:
+            var origin = CoordinatePosition;
+            
+            // if the player is already traveling a path, use the path's next destination as the origin
+            if (Path.Count > 0)
+                origin = Path.Peek();
+            
+            // set a new path:
+            Path = _pathfinder.FindPath(origin, destination);
+        }
+
+        public Queue<Vector2> Path { get; private set; }
+
         public override void Update(GameTime gameTime)
         {
+            var mouseState = Mouse.GetState();
+            var leftMouseButtonIsPressed = mouseState.LeftButton == ButtonState.Pressed;
+            if (!leftMouseButtonIsPressed && _leftMouseButtonWasPressed)
+            {
+                var mousePixelPosition = new Vector2(mouseState.X, mouseState.Y);
+                var displayCoordinate = Game.Display.CalculateCoordinateFromPixelPosition(mousePixelPosition);
+                var mapCoordinate = Game.Display.CalculateMapCoordinate(displayCoordinate);
+                Move(mapCoordinate);
+            }
+            _leftMouseButtonWasPressed = leftMouseButtonIsPressed;
+            
             var keyboardState = Keyboard.GetState();
 
             // if the sprite isn't already moving, accept new movement:
